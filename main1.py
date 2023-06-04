fd422ccfc7f5b26b071d065d6216497bb2b8178b
Sebastien Lionel Lubin <lubinsebastienlionel@gmail.com>
2023-06-04 02:04:05 -0300

    first

--- main1.py
+++ main1.py
@@ -2,39 +2,36 @@
 from afnd import construir_afnd
 from afd import determinizar_afnd
 from minimizar import minimizar_afd
+from tabulate import tabulate
 
 
 
 def salvar_afd(afd):
-    print('estados: {}'.format(','.join(estado.nome for estado in afd.estados)))
-    print('alfabeto: {}'.format(','.join(afd.alfabeto)))
-    print('estado_inicial: {}'.format(afd.estado_inicial))
-    print('estados_finais: {}'.format(','.join(estado for estado in afd.estados_finais)))
-    print()
+    headers = ["Estado", "Transição"]
+    table_data = []
+
     for estado in afd.estados:
         for transicao in estado.transicoes:
-            print('{} -{}-> {}'.format(estado.nome, transicao.simbolo, transicao.estado_destino))
+            table_data.append([estado.nome, transicao.simbolo, transicao.estado_destino])
 
+    # Add additional information to the table
+    table_data.append(["Estados:", ", ".join(estado.nome for estado in afd.estados)])
+    table_data.append(["Estados Finais:", afd.estados_finais])  # Remove the join() method
 
+    print(tabulate(table_data, headers=headers, tablefmt="grid"))
 
 
 # Função principal para execução do programa
 def main():
     arquivo_entrada = "arquivo_entrada.txt"
-
     # Construir o AFND a partir do arquivo de entrada
     afnd = construir_afnd(arquivo_entrada)
-    
-
     # Determinizar o AFND para obter o AFD
     afd = determinizar_afnd(afnd)
-
     # Minimizar o AFD
     afd_min = minimizar_afd(afd)
-
     # Imprimir o AFD
     salvar_afd(afd_min)
-
     print('Processamento concluído!')
 
 # Executar o programa
